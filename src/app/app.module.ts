f6ea2fb9b58d22b487d8a0d7d41cfdac142f96cb
kain <zhangtqx@vip.qq.com>
2019-05-31 11:16:14 +0800

    Init Submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
-import {registerLocaleData} from '@angular/common';
-import zh from '@angular/common/locales/zh';
-import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-registerLocaleData(zh);
-
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    HttpClientModule,
-    NgZorroAntdModule,
-    BrowserAnimationsModule,
-    BrowserModule.withServerTransition({appId: 'vanCMS'}),
+    BrowserModule
   ],
-  providers: [
-    {provide: NZ_I18N, useValue: zh_CN}
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
