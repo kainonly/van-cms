a934a53d1b3f6f2381dc94f437639d65c3875793
kain <zhangtqx@vip.qq.com>
2019-06-01 18:37:17 +0800

    Wait NgZorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import en from '@angular/common/locales/en';
+import {NgZorroAntdModule, NZ_I18N, en_US} from 'ng-zorro-antd';
 
 import {AppComponent} from './app.component';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'serverApp'})
+    NgZorroAntdModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
   ],
-  providers: [],
+  providers: [
+    {provide: NZ_I18N, useValue: en_US}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
