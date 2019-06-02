16f9e66c38ed83c27067a25a4323764dd3ab870e
kain <zhangtqx@vip.qq.com>
2019-06-02 13:53:01 +0800

    Use Angular Stable

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to van-cms!');
+    expect(page.getTitleText()).toEqual('Welcome to van-core!');
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {registerLocaleData} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
-import en from '@angular/common/locales/en';
-import {NgZorroAntdModule, NZ_I18N, en_US} from 'ng-zorro-antd';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
 
 registerLocaleData(en);
 
@@ -15,15 +16,13 @@ registerLocaleData(en);
     AppComponent
   ],
   imports: [
+    BrowserModule,
     NgZorroAntdModule,
+    FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    BrowserModule.withServerTransition({appId: 'serverApp'}),
+    BrowserAnimationsModule
   ],
-  providers: [
-    {provide: NZ_I18N, useValue: en_US}
-  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
