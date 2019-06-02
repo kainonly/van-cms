d4de2009724890350e2418d1e6382b15f90975c7
kain <zhangtqx@vip.qq.com>
2019-06-02 19:19:10 +0800

    Submit

---
+++ src/app/api/main.service.ts
@@ -0,0 +1,71 @@
+import {Injectable} from '@angular/core';
+import {HttpService} from 'ngx-bit';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+@Injectable()
+export class MainService {
+  private model = 'main';
+
+  constructor(private http: HttpService) {
+  }
+
+  /**
+   * Login
+   */
+  login(username: string, password: string): Observable<any> {
+    return this.http.req(this.model + '/login', {
+      username,
+      password
+    });
+  }
+
+  /**
+   * Token Verify
+   */
+  verify(): Observable<boolean> {
+    return this.http.req(this.model + '/verify');
+  }
+
+  /**
+   * get menu data
+   */
+  menu(): Observable<any> {
+    return this.http.req(this.model + '/menu').pipe(
+      map(res => {
+        const menu: any = {};
+        const nav = [];
+        const router: any = {};
+        if (!res.error) {
+          for (const x of res.data) {
+            menu[x.id] = x;
+            if (x.routerlink) {
+              router[x.routerlink] = x;
+            }
+          }
+          for (const x of res.data) {
+            if (!x.nav) {
+              continue;
+            }
+            if (x.parent === 0) {
+              nav.push(x);
+            } else {
+              const parent = x.parent;
+              if (menu.hasOwnProperty(parent)) {
+                const rows = menu[parent];
+                if (!rows.hasOwnProperty('children')) {
+                  rows.children = [];
+                }
+                rows.children.push(x);
+              }
+            }
+          }
+
+          return {menu, nav, router};
+        } else {
+          return {};
+        }
+      })
+    );
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {registerLocaleData} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 import en from '@angular/common/locales/en';
+import {NgZorroAntdModule, NZ_I18N, en_US} from 'ng-zorro-antd';
+import {PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarConfigInterface, PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
+import {NgxBitModule} from 'ngx-bit';
+import {environment} from '../environments/environment';
 
 registerLocaleData(en);
 
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path: '', loadChildren: './app.router.module#AppRouterModule', canActivate: [TokenService]},
+  {path: 'login', loadChildren: './login/login.module#LoginModule'},
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    NgZorroAntdModule,
-    FormsModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    NgZorroAntdModule,
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [
+    {provide: NZ_I18N, useValue: en_US}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
