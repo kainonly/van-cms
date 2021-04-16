032bc8a792bd522bbc85f212044187aa4a637258
kain <zhangtqx@vip.qq.com>
2021-04-16 21:19:55 +0800

    update

---
+++ schema/schema-type.ts
@@ -0,0 +1,23 @@
+export const SchemaType: any = {
+  0: {
+    label: {
+      zh_cn: '独立页面',
+      en_us: 'Pages'
+    },
+    value: 0
+  },
+  1: {
+    label: {
+      zh_cn: '文章页面',
+      en_us: 'Posts'
+    },
+    value: 1
+  },
+  2: {
+    label: {
+      zh_cn: '文章分栏',
+      en_us: 'Terms'
+    },
+    value: 2
+  }
+};
