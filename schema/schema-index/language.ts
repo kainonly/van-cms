d6ec777c4debf6e306a56dd92a697b298b690547
kain <zhangtqx@vip.qq.com>
2021-04-18 19:38:29 +0800

    update

---
+++ schema/schema-index/language.ts
@@ -0,0 +1,8 @@
+export default {
+  search: ['请输入您要搜索的展示名称', 'Please enter the display name'],
+  name: ['展示名称', 'Display Name'],
+  table: ['数据库表', 'Table'],
+  note: ['描述信息', 'Description'],
+  setting: ['设置', 'Setting'],
+  history: ['历史', 'History']
+};
--- schema/schema.module.ts
+++ schema/schema.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
+import { ShareModule } from 'van-skeleton';
+import { SchemaIndexComponent } from './schema-index/schema-index.component';
 
 @NgModule({
-  imports: [],
-  declarations: [],
-  exports: []
+  imports: [
+    ShareModule
+  ],
+  declarations: [
+    SchemaIndexComponent
+  ],
+  exports: [
+    SchemaIndexComponent
+  ]
 })
 export class SchemaModule {
 }
