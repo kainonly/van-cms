32db2b7ae25d2cc22ca0a0bdffdbf105cf3450d2
kain <zhangtqx@vip.qq.com>
2021-05-13 16:10:52 +0800

    update

--- media/public-api.ts
+++
@@ -1,4 +0,0 @@
-export * from './media.service';
-export * from './media-type.service';
-export * from './media.component';
-export * from './media.module';
--- schema/public-api.ts
+++
@@ -1,5 +0,0 @@
-export * from './schema.service';
-export * from './schema-index/schema-index.component';
-export * from './schema-add/schema-add.component';
-export * from './schema-edit/schema-edit.component';
-export * from './schema.module';
